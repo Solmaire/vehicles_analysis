6caf0958efffb6bafe78fe9cc1fca4782bdee270
Solmaire <solmaire.medina@gmail.com>
2024-05-25 19:27:47 -0400

    app.py version 1.0

--- app.py
+++ app.py
@@ -2,29 +2,31 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+
 car_data = pd.read_csv('vehicles_us.csv')
+
+st.header('Visualizador de Datos', divider="violet")
+dataviewer = st.checkbox('Mostrar Datos')
+describeviewer = st.checkbox('Mostrar estadisticas de datos')
+
+if dataviewer:
+    st.dataframe(car_data)
+
+if describeviewer:
+    st.dataframe(car_data.describe())
 
 st.header('Venta de vehículos')
 
 hist_button = st.button('Construir histograma')
+scatter_button = st.button('construir gráfico de dispersión')
 
 if hist_button:
-    st.write('Creación de un histograma precio-modelo')
+    st.subheader('Creación de un histograma precio-modelo')
+    his_car = px.histogram(car_data, y="price", x="model")
+    st.plotly_chart(his_car, use_container_width=True)
 
-    his_car = px.histogram(car_data, x="price", y="model")
-    his_car.show()
-
-    build_histogram = st.checkbox('Construir un histograma')
-
-    if build_histogram:
-        st.write('Construir un histograma para la columna precio-modelo')
-
-st.plotly_chart(his_car, use_container_width=True)
-
-scatter_button = st.button('construyendo gráfico de dispersión')
 
 if scatter_button:
-    st.write('Creación de gráfico de dispersión kilometros-precio')
-
+    st.subheader('Creación de gráfico de dispersión kilometros-precio')
     scatt_car = px.scatter(car_data, x="odometer", y="price")
-    scatt_car.show()
+    st.plotly_chart(scatt_car, use_container_width=True)
